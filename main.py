77bdb0ab2b8b2b68ad90f056c50eb3add8c74ccc
NoamRosenberg08 <noamrosenberg097@gmail.com>
2025-04-25 14:32:00 +0300

    added gitignore

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import mss
 import numpy as np
 from PIL import Image
 from mss.base import MSSBase
+import json
 
+def load_credentials_json(path: str):
+    return json.load(open(path, 'r'))
 
 def grab_screenshot(screenshooter: MSSBase, region: Dict = None) -> np.ndarray:
     if region is None:
@@ -26,5 +29,8 @@ region = {
 img = grab_screenshot(screenshooter=screenshooter, region=region)
 cv2.imwrite("current_screen.png", img)
 img = Image.open("C:\\Users\\control\\PycharmProjects\\Auto-Queueing\\current_screen.png")
+
+print(load_credentials_json('C:\\Users\\control\\PycharmProjects\\Auto-Queueing\\config.json')['api_key'])
+
 
 img.show()
