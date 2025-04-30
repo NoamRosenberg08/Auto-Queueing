edd677b4f0a9142583fa73ed54f40f2d4c7e38e8
NoamRosenberg08 <noamrosenberg097@gmail.com>
2025-04-30 12:53:28 +0300

    added capture match number screen and convert to binary for all of them

--- MatchNumberDetector.py
+++ MatchNumberDetector.py
@@ -3,7 +3,7 @@ from typing import Dict
 
 import cv2
 import mss
-from PIL import Image
+from PIL import Image, ImageFilter
 import numpy
 from numpy import ndarray
 from mss.base import MSSBase
@@ -11,21 +11,21 @@ from mss.base import MSSBase
 import ScreenShotUtils
 import pyautogui
 
-THRESHOLD: int = 120
+THRESHOLD: int = 80
 
 REGION = {
     "top": 255,
     "left": 1280,
-    "width": 100,
+    "width": 50,
     "height": 27
 }
 
-image: Image = Image.open(
-    "C:\\Users\\control\\PycharmProjects\\Auto-Queueing\\Screenshot 2025-04-27 135818.png").convert('L')
-image_array: ndarray = numpy.asarray(image)
+# image: Image = Image.open(
+#     "C:\\Users\\control\\PycharmProjects\\Auto-Queueing\\Screenshot 2025-04-27 135818.png").convert('L')
+# image_array: ndarray = numpy.asarray(image)
 
 
-def convert_image_to_grayscale(image_as_array: ndarray):
+def convert_image_to_binary(image_as_array: ndarray):
     return image_as_array > THRESHOLD
 
 
@@ -35,10 +35,19 @@ def screenshot_match_number(screenshooter: MSSBase):
 
 screenshooter = mss.mss()
 
-sleep(5)
-for i in range(10):
-    sleep(4)
-    print(f"capturing match {i}")
-    cv2img = screenshot_match_number(screenshooter=screenshooter)
-    cv2.imwrite(f"match_number{i}.png", cv2img)
-    pyautogui.hotkey("shift","n")
+def capture_all_match_numbers():
+    sleep(5)
+    for i in range(75):
+        sleep(3)
+        print(f"capturing match {i}")
+        cv2img = screenshot_match_number(screenshooter=screenshooter)
+        cv2.imwrite(f"match_number{i}.png", cv2img)
+        pyautogui.hotkey("shift", "n")
+
+def convert_all_match_numbers_images_to_grayscale():
+    for i in range(75):
+        grayscale_image : Image = Image.open(f"C:\\Users\\control\\PycharmProjects\\Auto-Queueing\\match_number{i}.png").convert('L')
+        binary_image : ndarray = convert_image_to_binary(numpy.asarray(grayscale_image))
+        Image.fromarray(binary_image).save(f"binary_match_number{i}.png")
+
+convert_all_match_numbers_images_to_grayscale()
