eb8e82dad072a5a365094b246ede9087ac7e5158
NoamRosenberg08 <noamrosenberg097@gmail.com>
2025-05-04 14:15:57 +0300

    added qual list for a team

--- FRCEventsAPIHandler.py
+++ FRCEventsAPIHandler.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 import requests
 from typing import Dict, List
 import json
@@ -18,15 +20,17 @@ FIT
 PCH
 FIM
 """
+
+
 @staticmethod
 def encode_credentials(username: str, token: str):
     return base64.b64encode((username + ":" + token).encode('ascii')).decode('ascii')
 
 class FRCEventsAPIHandler:
     def __init__(self, api_username: str, api_token: str, season_year: int):
-        self.base_url = "https://frc-api.firstinspires.org/v3.0/"+str(season_year)+"/"
+        self.base_url = "https://frc-api.firstinspires.org/v3.0/" + str(season_year) + "/"
         self.headers = {
-            'Authorization': "Basic " + encode_credentials(username=api_username,token=api_token),
+            'Authorization': "Basic " + encode_credentials(username=api_username, token=api_token),
             'If-Modified-Since': ''
         }
 
@@ -34,8 +38,7 @@ class FRCEventsAPIHandler:
 
     def get_request(self, endpoint: str):
         url = self.base_url + endpoint
-        response = requests.get(url, headers=self.headers,data={})
-
+        response = requests.get(url, headers=self.headers)
         if response.status_code == 200:
             return response.json()
         else:
@@ -48,9 +51,41 @@ class FRCEventsAPIHandler:
             event_code_list[event['code']] = event
         return event_code_list
 
+    def get_schedule_for_event(self, event_code: str) -> List[Dict]:
+        '''
+
+        :param event_code:
+        :return: List[Dict] every element of the list is data about this specific match.
+        note that list[0] = qual 1
+        '''
+
+        url = f"schedule/{event_code}?tournamentLevel=Qualification"
+        return self.get_request(url)['Schedule']
+
+
+    def is_team_in_match(self, match: dict, team_number: int):
+        for team in match['teams']:
+            if team['teamNumber'] == team_number:
+                return True
+        return False
+
+    def get_schedule_for_team(self, schedule : List[Dict], team_number: int) -> List[Dict]:
+        '''
+
+        :param schedule:
+        :param team_number:
+        :return: List[Dict] - every element of the list is a match. List[0] = qual 1
+        '''
+        teams_schedule: List[Dict] = []
+
+        for match in schedule:
+            if self.is_team_in_match(match, team_number):
+                teams_schedule.append(match)
+        return teams_schedule
+
 if __name__ == "__main__":
     # Example usage
-    #use the config.json file to get the api username and token
+    # use the config.json file to get the api username and token
     with open('config.json') as config_file:
         config = json.load(config_file)
         api_username = config['FRCEvents']['username']
@@ -60,3 +95,7 @@ if __name__ == "__main__":
 
     frc_api_handler = FRCEventsAPIHandler(api_username, api_token, season_year)
     events = frc_api_handler.get_event_list("ISR")
+    # print(events)
+    schdule = frc_api_handler.get_schedule_for_event('ISCMP')
+    # print(schdule)
+    print(frc_api_handler.get_schedule_for_team(schdule, 4590))
