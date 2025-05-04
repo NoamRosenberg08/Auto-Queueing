a04b252423b4f8f3004436815642bb0a630d6c63
NoamRosenberg08 <noamrosenberg097@gmail.com>
2025-05-04 20:57:30 +0300

    made qual list bigger

--- main.py
+++ main.py
@@ -73,24 +73,24 @@ def should_team_go_to_queue():
 def get_schedule_for_team():
     return jsonify(match_dict)
 
-
-def update_match_info():
-    global match_time, match_number
-    screenshooter : MSSBase = mss.mss()
-
-    match_time_finder: MatchTimeFinder = MatchTimeFinder.MatchTimeFinder(roboflow_api, "timedetectionv2", 5, screenshooter)
-    match_number_finder: MatchNumberFinder = MatchNumberFinder.MatchNumberFinder(roboflow_api, "matchdetectionv2", 1,screenshooter)
-
-    while True:
-        screenshooter = mss.mss()
-        if match_time == 0:
-            match_number = match_number_finder.get_match_number()
-        match_time = match_time_finder.get_match_time()
-        print("Current match time:", match_time)
-        time.sleep(1)  # Sleep to avoid excessive CPU usage
-
-background_thread = threading.Thread(target=update_match_info, daemon=True)
-background_thread.start()
+#
+# def update_match_info():
+#     global match_time, match_number
+#     screenshooter : MSSBase = mss.mss()
+#
+#     match_time_finder: MatchTimeFinder = MatchTimeFinder.MatchTimeFinder(roboflow_api, "timedetectionv2", 5, screenshooter)
+#     match_number_finder: MatchNumberFinder = MatchNumberFinder.MatchNumberFinder(roboflow_api, "matchdetectionv2", 1,screenshooter)
+#
+#     while True:
+#         screenshooter = mss.mss()
+#         if match_time == 0:
+#             match_number = match_number_finder.get_match_number()
+#         match_time = match_time_finder.get_match_time()
+#         print("Current match time:", match_time)
+#         time.sleep(1)  # Sleep to avoid excessive CPU usage
+#
+# background_thread = threading.Thread(target=update_match_info, daemon=True)
+# background_thread.start()
 
 def run_flask():
     app.run(debug=True, use_reloader=False)
